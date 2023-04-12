8acd9871064ceda9baa4d88e686c455d0474d84e
richardhanslie <richardhanslie@gmail.com>
2023-04-12 15:20:06 +0700

    first commit

---
+++ src/test/java/id/co/nanoproject/PaymentGateway/PaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.nanoproject.PaymentGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
